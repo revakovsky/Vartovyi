e1f8f848aa7f0ef9b0dfc8189165c05e5b30d416
@revakovsky <revakovsky@gmail.com>
2026-03-09 17:11:49 +0200

    add bottom and top bars

---
+++ app/src/main/java/com/revakovskyi/vartovyi/navigation/BottomNavItem.kt
@@ -0,0 +1,41 @@
+package com.revakovskyi.vartovyi.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.revakovskyi.vartovyi.R
+
+sealed class BottomNavItem(
+    val route: Routes,
+    @param:StringRes val labelResId: Int,
+    @param:DrawableRes val iconResId: Int,
+) {
+
+    data object Home : BottomNavItem(
+        route = Routes.Home,
+        labelResId = R.string.nav_home,
+        iconResId = R.drawable.home,
+    )
+
+    data object Keywords : BottomNavItem(
+        route = Routes.Keywords,
+        labelResId = R.string.nav_keywords,
+        iconResId = R.drawable.words,
+    )
+
+    data object Logs : BottomNavItem(
+        route = Routes.Log,
+        labelResId = R.string.nav_log,
+        iconResId = R.drawable.logs,
+    )
+
+    data object Settings : BottomNavItem(
+        route = Routes.Settings,
+        labelResId = R.string.nav_settings,
+        iconResId = R.drawable.settings,
+    )
+
+    companion object {
+        val all: List<BottomNavItem> by lazy { listOf(Home, Keywords, Logs, Settings) }
+    }
+
+}
